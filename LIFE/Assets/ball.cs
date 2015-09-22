0409a836116e65319018f479ff1d2634a218a652
Li Yang <yangl395@newschool.edu>
2015-09-22 12:21:15 -0400

    Week3

---
+++ LIFE/Assets/ball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+	private float speed;
+	// Use this for initialization
+	void Start () {
+		float _s = Random.value;
+		transform.localScale = new Vector3 (_s,_s,_s);
+		gameObject.GetComponent<Rigidbody> ().AddForce (new Vector3 (rdn(), 0, rdn()));
+	}
+	float rdn(){
+		return (Random.value - 0.5f) * 20;
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ LIFE/Assets/spawnExploding.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnExploding : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject newBall;
+	private float speed = 5f;
+	private int numBall = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	Vector3 rdnPosition(){
+		return new Vector3 (Random.value * 12 - 6, 0, Random.value * 12 - 6);
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Random.value < 0.004 && numBall<=20) {
+			numBall++;
+			Instantiate (newBall, rdnPosition(),transform.rotation);
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			Go(1);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			Go(2);
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			Go(3);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			Go(4);
+		}
+
+	}
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "bad")
+		{
+			explode();
+		}
+	}
+	void Go(int d){
+		switch (d) {
+		case 1:
+			gameObject.GetComponent<Rigidbody> ().AddForce (new Vector3 (-speed, 0, 0));
+			break;
+		case 2:
+			gameObject.GetComponent<Rigidbody> ().AddForce (new Vector3 (speed, 0, 0));
+			break;
+		case 3:
+			gameObject.GetComponent<Rigidbody> ().AddForce (new Vector3 (0,0, speed));
+			break;
+		case 4:
+			gameObject.GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 0,-speed));
+			break;
+		}
+	}
+			
+
+	void explode(){
+		Instantiate (explosion, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+
+}
